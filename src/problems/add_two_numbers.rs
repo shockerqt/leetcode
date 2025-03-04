902d489f4d3be10206994c11519618120aedebef
shocker <fjana@gmail.com>
2025-03-04 19:55:52 -0300

    Add project with first problem

---
+++ src/problems/add_two_numbers.rs
@@ -0,0 +1,30 @@
+use super::Solution;
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+impl Solution {
+    pub fn add_two_numbers(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+    }
+}
+
+pub fn example() {
+    // [2,4,3]
+    let l1 = ListNode::new(?);
+    // [5,6,4]
+    let l2 = ListNode::new(?);
+}
---
+++ src/problems/mod.rs
@@ -0,0 +1,4 @@
+pub mod add_two_numbers;
+pub mod two_sum;
+
+pub struct Solution;
